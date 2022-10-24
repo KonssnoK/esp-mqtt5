78110697ddafe9db49b301295617101070b61b01
lorenzo.consolaro <lorenzo.consolaro@tiko.energy>
2022-10-24 14:16:46 +0200

    [mqtt] Bugfix

--- mqtt_client.c
+++ mqtt_client.c
@@ -1558,7 +1558,7 @@ esp_err_t esp_mqtt_client_start(esp_mqtt_client_handle_t client)
     }
     MQTT_API_LOCK(client);
     if (client->state != MQTT_STATE_INIT && client->state != MQTT_STATE_DISCONNECTED) {
-        ESP_LOGE(TAG, "Client has started");
+        ESP_LOGE(TAG, "Client already started: %d", client->state);
         MQTT_API_UNLOCK(client);
         return ESP_FAIL;
     }
@@ -1646,9 +1646,9 @@ esp_err_t esp_mqtt_client_stop(esp_mqtt_client_handle_t client)
         }
 
         client->run = false;
-        client->state = MQTT_STATE_DISCONNECTED;
         MQTT_API_UNLOCK(client);
         xEventGroupWaitBits(client->status_bits, STOPPED_BIT, false, true, portMAX_DELAY);
+        client->state = MQTT_STATE_DISCONNECTED;
         return ESP_OK;
     } else {
         ESP_LOGW(TAG, "Client asked to stop, but was not started");
